d200cfaf74e36c5cec57f2897729c10bae2263c6
luckyalade <luckyalade309@gmail.com>
2023-12-16 17:24:15 +0100

    Basic Unit Test

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import SignUp
 # Create your tests here.
+
+
+class SignUpModelTest(TestCase):
+
+  @classmethod
+  def setUpTestData(cls):
+    cls.signup = SignUp.objects.create(
+        firstName="Lucky",
+        lastName="Alade"
+    )
+
+  def test_field(self):
+    self.assertIsInstance(self.signup.firstName, str)
+    self.assertIsInstance(self.signup.lastName, str)
